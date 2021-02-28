9bd90609a83f8923bb191698a9dba912e14bb1bd
huro3h <huro3h@gmail.com>
2021-02-28 11:23:30 +0900

    Assemble html with js

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ document.body.appendChild(button)
 // const userId = 'huro3h';
 const githubApiEndPoint = 'https://api.github.com/users/'
 
+const view = `
+<h6>${userInfo.name} (@${userInfo.login})</h6>
+<img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="75">
+<dl>
+  <dt>Location</dt>
+  <dd>${userInfo.location}</dd>
+  <dt>Repositries</dt>
+  <dd>${userInfo.public_repos}</dd>
+</dl>
+`;
+  
 function fetchUserInfo(userId) {
   fetch(`${githubApiEndPoint}${encodeURIComponent(userId)}`)
     .then(response => {
