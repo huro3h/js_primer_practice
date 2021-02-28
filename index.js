5a7ddb34c82758045ad0472b904eba25e91ee282
huro3h <huro3h@gmail.com>
2021-02-28 10:34:17 +0900

    Update fetch process as function

--- index.js
+++ index.js
@@ -11,19 +11,22 @@ document.body.appendChild(button)
 const userId = 'huro3h';
 const githubApiEndPoint = 'https://api.github.com/users/'
 
-fetch(`${githubApiEndPoint}${encodeURIComponent(userId)}`)
-  .then(response => {
-    console.log(response.status);
+function fetchUserInfo(userId) {
+  fetch(`${githubApiEndPoint}${encodeURIComponent(userId)}`)
+    .then(response => {
+      console.log(response.status);
 
-    if(!response.ok) {
-      console.error('エラーレスポンス', response);
-    } else {
-      return response.json().then(userInfo => {
-        // JSONパースされたオブジェクト
-        console.log(userInfo);
-      });
-    }
-  }).catch(error => {
+      if(!response.ok) {
+        console.error('エラーレスポンス', response);
+      } else {
+        return response.json().then(userInfo => {
+          // JSONパースされたオブジェクト
+          console.log(userInfo);
+        });
+      }
+    }).catch(error => {
     console.error(error);
-});
+  });
+}
+
 
