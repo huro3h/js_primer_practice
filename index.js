e4320e56b29ab9c8cc23924e18f05258e5fe56a5
huro3h <huro3h@gmail.com>
2021-02-28 10:12:41 +0900

    Add headind text, button

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('index.js: loaded');
+// console.log('index.js: loaded');
+
+const heading = document.querySelector('h5');
+
+const headingText = heading.textContent;
+
+const button = document.createElement('button');
+button.textContent = '押す';
+document.body.appendChild(button)
