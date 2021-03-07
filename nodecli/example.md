6b125d8f29b385e9ce5924fba07f5d81aed5f13a
huro3h <huro3h@gmail.com>
2021-03-07 18:25:31 +0900

    Read exmaple.md

---
+++ nodecli/example.md
@@ -0,0 +1,2 @@
+exmaple.com
+sample2.example.com
--- nodecli/main.js
+++ nodecli/main.js
@@ -1,3 +1,12 @@
-// process.argv コンソールにコマンドライン引数を出力
-console.log(process.argv);
+// commanderモジュールをオブジェクトとしてインポート
+const program = require('commander');
 
+// process.argv コンソールにコマンドライン引数を出力
+// console.log(process.argv);
+
+// コマンドライン引数をパース
+program.parse(process.argv);
+
+const filePath = program.args[0];
+
+console.log(filePath);
