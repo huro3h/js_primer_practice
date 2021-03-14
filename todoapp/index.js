9714944e0195135f1ba9483516405f5d64894e03
huro3h <huro3h@gmail.com>
2021-03-14 18:32:49 +0900

    Feature/chapter32 (#4)
    * Add entry point files for todoapp
    
    * Modularize App class
    
    * Inbitialize App
    
    * test
    
    * Initialize App
    
    * test1
    
    * test2
    
    * test3
    
    * remove debug code

---
+++ todoapp/index.js
@@ -0,0 +1,5 @@
+import { App } from "./src/App.js"
+const app = new App();
+
+console.log('loaded index.js');
+console.log('start cmd yarn js-primer-local-server --port 8000');
---
+++ todoapp/src/App.js
@@ -0,0 +1,6 @@
+console.log('loaded App.js');
+export class App {
+  constructor() {
+    console.log('App initialized');
+  }
+}
