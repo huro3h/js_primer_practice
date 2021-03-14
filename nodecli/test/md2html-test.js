b4262a0374a1b9cf6912dbb73c33f2e6bcdf8eb9
huro3h <huro3h@gmail.com>
2021-03-14 10:44:32 +0900

    Add test, convert into html from markdown (gfm: true)

--- nodecli/test/md2html-test.js
+++ nodecli/test/md2html-test.js
@@ -13,3 +13,14 @@ it("マークダウンからHTMLに変換できること(gfm: false)", () => {
 
   assert.strictEqual(md2html(sample, { gfm: false }), expected);
 });
+
+it("マークダウンからHTMLに変換できること(gfm: true)", () => {
+  // fs.readFileSyncは同期的にファイルを読み込むメソッド
+  const sample = fs.readFileSync(path.resolve(__dirname,
+    "./fixtures/sample.md"), { encoding: "utf-8" });
+
+  const expected = fs.readFileSync(path.resolve(__dirname,
+    "./fixtures/expected-gfm.html"), { encoding: "utf-8" });
+
+  assert.strictEqual(md2html(sample, { gfm: true }), expected);
+});
