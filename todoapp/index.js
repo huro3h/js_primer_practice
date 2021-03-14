3e4e187b28534a82644fc084f34867961a64a7b4
huro3h <huro3h@gmail.com>
2021-03-14 18:49:51 +0900

    Displayed input value to console

--- todoapp/index.js
+++ todoapp/index.js
@@ -1,5 +1,6 @@
 import { App } from "./src/App.js"
 const app = new App();
+app.mount();
 
-console.log('loaded index.js');
-console.log('start cmd yarn js-primer-local-server --port 8000');
+// console.log('loaded index.js');
+// console.log('start cmd yarn js-primer-local-server --port 8000');
--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,6 +1,14 @@
-console.log('loaded App.js');
+// console.log('loaded App.js');
+
 export class App {
-  constructor() {
-    console.log('App initialized');
+  mount() {
+    const formElement = document.querySelector('#js-form');
+    const inputElement = document.querySelector('#js-form-input');
+
+    formElement.addEventListener('submit', (event) => {
+      // submitイベントの本来の動きを止める
+      event.preventDefault();
+      console.log(`入力値: ${inputElement.value}`);
+    });
   }
 }
