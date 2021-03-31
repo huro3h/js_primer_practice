a4f8bee07be562599f1cf6a80fe342784669012b
huro3h <huro3h@gmail.com>
2021-03-31 15:59:23 +0900

    example EventEmitter

--- todoapp/index.js
+++ todoapp/index.js
@@ -1,6 +1,14 @@
-import { App } from "./src/App.js"
-const app = new App();
-app.mount();
+// import { App } from "./src/App.js"
+// const app = new App();
+// app.mount();
 
 // console.log('loaded index.js');
 // console.log('start cmd yarn js-primer-local-server --port 8000');
+
+import { EventEmitter } from "./src/EventEmitter.js";
+const event = new EventEmitter();
+
+event.addEventListener('test-event', () => console.log('1'));
+event.addEventListener('test-event', () => console.log('100'));
+
+event.emit('test-event');
